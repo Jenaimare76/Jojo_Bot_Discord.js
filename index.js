0c21003036eaf4c5e1480f0ab6ad778ee8e66738
Jeanaimare76 <37538097+Jenaimare76@users.noreply.github.com>
2018-04-15 17:00:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+//variabales
+var prefix = ("@Jojo")
+
+//CONNECTION DU BOT
+    bot.on('ready', function () {
+
+    //ACTIVITE DU BOT
+        bot.user.setGame("@Jojo help")
+        console.log("je suis de retour ");
+    });
+
 //connection du bot
 bot.login(process.env.TOKEN);
