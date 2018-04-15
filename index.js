303d5f0de980cb3a6110164a469da7d9f375f4cb
Jeanaimare76 <37538097+Jenaimare76@users.noreply.github.com>
2018-04-15 16:33:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 
 //connection du bot
-bot.login('NDIzODg3NzA1MDY0MDc5MzYw.DbTgXA.B5pxJW4fdXRoHWGoYHszpk7Uw8I');
+bot.login(TOKEN);
