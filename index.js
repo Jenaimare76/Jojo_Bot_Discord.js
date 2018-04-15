ee93c63b22b9098450d33b6047ecf86428143713
Jeanaimare76 <37538097+Jenaimare76@users.noreply.github.com>
2018-04-15 18:01:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,11 @@
         if (message.content === ('!ping'){
             channel.reply('!pong')
             }
-        })
-
-     bot.on('message', function (message) {
+        
         if (message.content === ('!pong'){
             channel.reply('!ping')
             }
         })
-        
+            
 //connection du bot
     bot.login(process.env.TOKEN);
