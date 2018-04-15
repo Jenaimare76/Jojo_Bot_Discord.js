942f2559ab8adc3e347986f153ea2f7582661be3
Jeanaimare76 <37538097+Jenaimare76@users.noreply.github.com>
2018-04-15 22:43:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
     const bot = new Discord.Client();
 
 //variabales
-    var prefix = ("@" + "Jojo ") 
+    var prefix = ("@" + "Jojo ")
+    var time = time
 
 //CONNECTION DU BOT
     bot.on('ready', function () {
 
     //ACTIVITE DU BOT
-        bot.user.setGame("@" + "Jojo" + " help");
         console.log("je suis de retour ");
+        if (time = 20 (bot.user.setGame("@"+ "Jojo" + " help")))
+        if (time = 0 (bot.user.setGame("https://discord.gg/NaDhCMt")))
     });
 
 //connection du bot
