c6594bd6590b2607b90c91a3ab0319e0ac38e782
Jeanaimare76 <37538097+Jenaimare76@users.noreply.github.com>
2018-04-15 16:57:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
 //connection du bot
 bot.login(process.env.TOKEN);
