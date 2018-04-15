16031486fcd72f4d11bff2188a2c30997b660121
Jeanaimare76 <37538097+Jenaimare76@users.noreply.github.com>
2018-04-15 22:55:13 +0200

    Update bruit.js

--- Commandes/bruit.js
+++ Commandes/bruit.js
@@ -1,2 +1,17 @@
 const commandes = require ("./Commandes")
-const Discord = require ("discord.js")
+const Discord = require ("discord.js").
+
+var prefix = ("@"+"Jojo ")
+
+if (message.content === prefix +  "omg") {
+  
+   message.delete();
+            if (message.member.voiceChannel === undefined) return message.reply(wrap("Vous n\'êtes pas dans un channel vocal !"));
+            voiceChannel.join().then(connection =>{
+                const dispatcher = connection.playFile('./Audio/omg.mp3');
+                dispatcher.on("end", end => {
+                    voiceChannel.leave();
+                });
+            })
+            .catch(err => console.log(err)); 
+}
