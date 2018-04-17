645e326d628d773e7fd6ee8c69b8fd7e78652d97
Jeanaimare76 <37538097+Jenaimare76@users.noreply.github.com>
2018-04-17 16:01:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,17 @@
 
 //connection du bot
     bot.login(process.env.TOKEN);          
+
+//COMMANDE DU SON "OMG"    
+     if (message.content === prefix +  "omg") {
+ 
+         message.delete();
+         if (message.member.voiceChannel === undefined) return message.reply(wrap("Vous n\'êtes pas dans un channel vocal !"));
+         voiceChannel.join().then(connection =>{
+             const dispatcher = connection.playFile('./music/omg.mp3');
+             dispatcher.on("end", end => {
+                 voiceChannel.leave();
+             });
+         })
+         .catch(err => console.log(err));  
+     }
