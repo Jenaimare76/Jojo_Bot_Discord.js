184c4e90f874565e660b3d9b156c7cca6a488c4f
Jeanaimare76 <37538097+Jenaimare76@users.noreply.github.com>
2018-04-22 19:24:36 +0200

    Update taz.js

--- Commandes/bruit/taz.js
+++ Commandes/bruit/taz.js
@@ -1,4 +1,20 @@
-class Taz {
+const Commande = require('./Commandes/CommandeMap')
 
+module.exports =class taz extends Commande{
+    
+  static match (message) {
+    return message.content.startsWith('@Jojo taz')
+  }
+  
+  static action (message) {
+    let voiceChannel = message.guild.channels
+      .filter(function (channel) { return channel.type === 'voice'})
+      .first()
+    voiceChannel
+      .join()
+      .then(function (connection) {
+        connection.playFile('./bruit/tazer.mp3')
+    })
+  }
   
 }
