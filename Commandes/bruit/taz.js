ad4cf530075fc66c8e5c215807b0e576c9b5dbd8
Jeanaimare76 <37538097+Jenaimare76@users.noreply.github.com>
2018-04-22 20:13:03 +0200

    Update taz.js

--- Commandes/bruit/taz.js
+++ Commandes/bruit/taz.js
@@ -1,4 +1,4 @@
-const Commande = require('Jojo_Bot_Discord.js/Commandes/CommandMap.js')
+const Commande = require('Jeanaimare76/Jojo_Bot_Discord.js/Commandes/CommandMap.js')
 
 module.exports =class taz extends Commande{
     
